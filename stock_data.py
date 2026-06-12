a18bbdc67f497f83162f6a1b113f15705b8e72ff
Yash <ymudgal4@gmail.com>
2026-06-13 01:11:35 +0530

    Added Sharpe ratio and max drawdown metrics

--- stock_data.py
+++ stock_data.py
@@ -10,6 +10,8 @@ data["MA50"] = data["Close"].rolling(50).mean()
 
 data["Signal"] = 0
 
+# Signal generation
+
 data.loc[data["MA20"] > data["MA50"], "Signal"] = 1
 data["Position"] = data["Signal"]
 data["Returns"] = data["Close"].pct_change()
@@ -18,6 +20,8 @@ data["Strategy_Returns"] = (
     data["Position"].shift(1)
     *data["Returns"]
 )
+
+#portfolio value calculation
 
 data["Portfolio"] = (
     1 + data["Strategy_Returns"]
@@ -29,11 +33,27 @@ print("Rows remaining:", len(data))
 final_value = data["Portfolio"].iloc[-1]
 print("Final Portfolio Value: ", final_value)
 
+#return calculation
+
 total_return = (
     (final_value - initial_cash)/initial_cash
 )*100
 
+#sharpe ratio calculation
+
+sharpe = (
+    data["Strategy_Returns"].mean()/data["Strategy_Returns"].std()
+)* (252**0.5)
+
+print(f"Sharpe: {sharpe:.2f}")
 print(f"Total Return: {total_return:.2f}%")
+
+rolling_max = data["Portfolio"].cummax()
+
+drawdown = (data["Portfolio"] - rolling_max)/rolling_max
+
+max_drawdown = drawdown.min()
+print(f"Max Drawdown: {max_drawdown:.2%}")
 
 print(data[["Close",
             "Signal",
@@ -42,4 +62,3 @@ print(data[["Close",
             "Strategy_Returns",
             "Portfolio"
 ]].tail(10))
-
